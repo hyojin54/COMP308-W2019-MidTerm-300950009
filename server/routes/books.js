72109cb8542f31c6cf4491f2b7cd1fc899303d12
Hyojin Kim <akumada54@gmail.com>
2019-02-25 14:28:18 -0500

    fixed Add Button, and added Edit and Delete Buttons

--- server/routes/books.js
+++ server/routes/books.js
@@ -9,7 +9,7 @@ let book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
     }
@@ -25,11 +25,10 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  res.render('books/details', {
+    title: 'Book Details',
+    books: ''
+  });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -43,10 +42,19 @@ router.post('/add', (req, res, next) => {
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  book.findById(id, (err, book) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/details', {
+        title: 'Book Details',
+        books: book
+      });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
@@ -60,10 +68,17 @@ router.post('/:id', (req, res, next) => {
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  book.remove({_id: id}, (err) => {
+    if (err) {
+      return console.error(err);
+    } 
+    else {
+      res.redirect('/books');
+    }
+  });
+
 });
 
 
